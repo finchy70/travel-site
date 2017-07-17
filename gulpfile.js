d9ca4735a1a49514660c0c62aa87fa1c5ad18c03
finchy70 <finchy70@gmail.com>
2017-07-17 19:37:01 +0100

    Finished gulp css file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,34 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
 
-gulp.task('default', function() {
-  console.log('Success');
+
+
+    gulp.task('default', function() {
+    console.log('Success');
 });
 
 gulp.task('html', function() {
-  console.log('Success - HTML Task');
+    console.log('Success - HTML Task');
 });
 
 gulp.task('styles', function(){
-  console.log('Success - CSS Stuff');
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, autoprefixer, nested]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
+
 
 gulp.task('watch', function(){
 
-  watch('./app/index.html', function(){
-  gulp.start('html');
-  });
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
 
-  watch('./app/assets/styles/styles.css', function(){
-  gulp.start('styles');
-  });
+    watch('./app/assets/styles/styles.css', function(){
+        gulp.start('styles');
+    });
 });
